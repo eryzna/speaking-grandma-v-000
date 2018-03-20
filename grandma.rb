8ea18d5681afd2f48fa49201a8be2ccb493df5ac
Elana Ryznar <eryzna@gmail.com>
2018-03-20 16:57:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma (phrase)
   puts "HUH?! SPEAK UP, SONNY!"
   if phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  else phrase="I LOVE YOU GRANDMA!"
+  elsif phrase="I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
