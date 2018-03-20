cf1e7ba84bdc9665ed3cc0ec15a781e7cf822805
Elana Ryznar <eryzna@gmail.com>
2018-03-20 16:51:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,8 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma (phrase)
+  if phrase=="I love you grandma!".uppercase
+    puts "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
