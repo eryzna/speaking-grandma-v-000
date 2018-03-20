600ac7c255ebad958b89e4a27bcb084e36156d19
Elana Ryznar <eryzna@gmail.com>
2018-03-20 17:22:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@ def speak_to_grandma (phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase=="Hi Nana, how are you?"
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase== "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
